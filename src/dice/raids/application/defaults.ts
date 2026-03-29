330d7b3c10a90e0ec12a96d273472972cbeb3abd
Tero Laanti <laanti.tero@gmail.com>
2026-03-29 20:39:53 +0300

    Improve raid recruitment handoff and expiry UX

--- src/dice/raids/application/defaults.ts
+++ src/dice/raids/application/defaults.ts
@@ -1,3 +1,3 @@
 export const raidPartySizeLimit = 4;
 export const raidRecruitmentDurationMs = 15 * 60 * 1000;
-export const raidEncounterDurationMs = 12 * 60 * 1000;
+export const raidEncounterDurationMs = 5 * 60 * 1000;
